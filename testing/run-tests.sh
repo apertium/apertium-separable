fdc1f0f8efc5cec6774beee3424be3d7022e6d9c
Francis M. Tyers <ftyers@prompsit.com>
2017-06-23 19:01:13 +0000

    more stufff
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/apertium-separable@79537 72bbbca6-d526-0410-a7d9-f06f51895060

--- testing/run-tests.sh
+++ testing/run-tests.sh
@@ -1,2 +1,10 @@
 
+CORP=../corpus/
+LANGS=english
+
+for lang in $LANGS; do 
+
+	cat $CORP"/"$lang.src > $CORP"/"$lang.tst
+	python3 evaluate.py $CORP"/"$lang.src $CORP"/"$lang.ref $CORP"/"$lang.tst
+done
 
