5ac1923c3b1414951c372cbab6dae8a6d947b2f9
Francis M. Tyers <ftyers@prompsit.com>
2017-06-23 19:11:04 +0000

    some comments
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/apertium-separable@79539 72bbbca6-d526-0410-a7d9-f06f51895060

--- testing/run-tests.sh
+++ testing/run-tests.sh
@@ -3,8 +3,12 @@ CORP=../corpus/
 LANGS=english
 
 for lang in $LANGS; do 
+	# Just copy input to output, but this is where your module is going to go
+	# you're going to do cat $CORP"/"$lang.src | your-module.py DATA > $CORP"/"$lang.tst
 
 	cat $CORP"/"$lang.src > $CORP"/"$lang.tst
+
+	# Run the evaluation
 	python3 evaluate.py $CORP"/"$lang.src $CORP"/"$lang.ref $CORP"/"$lang.tst
 done
 
