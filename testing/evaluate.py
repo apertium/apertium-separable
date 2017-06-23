a18ea804dc4cb7d34f005aed64ba7effa5646de1
Francis M. Tyers <ftyers@prompsit.com>
2017-06-23 19:07:27 +0000

    add evaluation script
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/apertium-separable@79538 72bbbca6-d526-0410-a7d9-f06f51895060

--- testing/evaluate.py
+++ testing/evaluate.py
@@ -1,1 +1,30 @@
 import sys;
+
+src = open(sys.argv[1]);
+ref = open(sys.argv[2]);
+tst = open(sys.argv[3]);
+
+hit = 0.0;
+miss = 0.0;
+
+while True: #{
+
+
+	src_l = src.readline();
+	ref_l = ref.readline();
+	tst_l = tst.readline();
+
+	if src_l.strip() == '' or ref_l.strip() == '' or tst_l.strip() == '':  #{
+		break;
+	#}
+
+	if tst_l == ref_l: #{
+		hit += 1.0
+	else: #{
+		miss += 1.0
+	#}
+
+#}
+
+print('total: %d' % (hit+miss));
+print('accur: %.2f%%' % (hit/(hit+miss)*100.0));
