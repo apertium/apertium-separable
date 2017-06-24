91c2038a7ed8ecff39bb8c8d0ea13d349b8c1bbe
Francis M. Tyers <ftyers@prompsit.com>
2017-06-24 06:58:31 +0000

    +1 example
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/apertium-separable@79597 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ examples/convert-mwe-att.py
@@ -0,0 +1,37 @@
+import sys;
+
+for line in sys.stdin.readlines(): #{
+
+	if line.strip() == '': #{
+		continue
+	#}
+	
+	row = line.strip('\n').split('\t');
+
+	# Final state
+	if len(row) == 1: #{
+		print(row[0]);
+		continue;
+	#}
+	
+	# Replace word boundary symbol with the special one
+	if row[2] == '+': #{
+		row[2] = '<$>';
+	#}
+	if row[3] == '+': #{
+		row[3] = '<$>';
+	#}
+	
+	# Replace ~ symbol with <ANY_TAG> and add a loop
+	if row[2] == '~': #{
+		row[2] = '<ANY_TAG>';
+		print('%s\t%s\t%s\t%s' % (row[1], row[1], row[2], 'ε'));
+	#}
+	if row[3] == '~': #{
+		row[3] = '<ANY_TAG>';
+		print('%s\t%s\t%s\t%s' % (row[1], row[1], 'ε', row[3]));
+	#}
+
+
+	print('\t'.join(row));
+#}
