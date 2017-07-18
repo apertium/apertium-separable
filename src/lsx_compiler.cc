9e597ecd3aee713397fb153953094648d7a8072c
Francis M. Tyers <ftyers@prompsit.com>
2017-07-18 17:18:13 +0000

    rename file
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/apertium-separable@80709 72bbbca6-d526-0410-a7d9-f06f51895060

--- src/lsx_compiler.cc
+++ src/lsx_compiler.cc
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
-#include <compiler.h>
+#include <lsx_compiler.h>
 #include <lttoolbox/compression.h>
 #include <lttoolbox/entry_token.h>
 #include <lttoolbox/lt_locale.h>
