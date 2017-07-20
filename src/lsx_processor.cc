884e78169a3351ce2975ed7409e26d919c67c993
Irene Tang <itang1@swarthmore.edu>
2017-07-20 05:19:20 +0000

    fixed print spacing/formatting, handles leading/trailing chars, breaks when no match
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/apertium-separable@80787 72bbbca6-d526-0410-a7d9-f06f51895060

--- src/lsx_processor.cc
+++ src/lsx_processor.cc
@@ -105,126 +105,156 @@ int main (int argc, char** argv)
   bool outOfWord = true;
   bool isEscaped = false;
   bool finalFound = false;
+  bool reset = false;
 
-  wstring in, out;
+  wstring in = L"";
+  wstring out;
 
+  bool leading = false;
+  // int i=0;
   while(!feof(input))
   {
-      if(alive_states.size() == 0 && !finalFound)
-      {
-        alive_states.push_back(*initial_state);
-        fputws(in.c_str(), output);
-        in = L"";
-      }
+    // i++;
+    int val = fgetwc(input); // read 1 wide char
+    // cout << i << " " << &alive_states[0] << endl;
+    // cout << &alive_states.front() << " " << initial_state << endl << endl;
+    if(alive_states.size() == 0 && !finalFound)
+    {
+      // wcout << "val: " << (wchar_t)val << endl;
+      alive_states.push_back(*initial_state);
+      // fputws(L"IN",output);
+      fputws(in.c_str(), output);
+      in = L"";
+      leading = true;
+    }
+    else if(alive_states.size() == 0 && finalFound)
+    {
+      in = L"";
+      finalFound = false;
+    }
+    else if(alive_states.size() == 1 && &alive_states.front() == initial_state)
+    {
+      // fputws(in.c_str(), output);
+      // in = L"";
+      // cout <<"Here"<<endl;
+      // finalFound = false;
+    }
 
-      int val = fgetwc(input); // read 1 wide char
-      // wcout << L"| " << (wchar_t)val << L" | val: " << val << L" || as.size(): " << alive_states.size() << L" || out of word: " << outOfWord << endl;
+    // cout << "val: " << (char) val << endl;
+    // wcout << L"| " << (wchar_t)val << L" | val: " << val << L" || as.size(): " << alive_states.size() << L" || out of word: " << outOfWord << endl;
 
-      if((val == L'^' && !isEscaped && outOfWord))
-      {
-        outOfWord = false;
-        in += val;
-        continue;
-      }
+    if((val == L'^' && !isEscaped && outOfWord))
+    {
+      outOfWord = false;
+      in += val;
+      continue;
+    }
 
-      if((feof(input) || val == L'$') && !isEscaped && !outOfWord)
+    if((feof(input) || val == L'$') && !isEscaped && !outOfWord)
+    {
+      new_states.clear();
+      for(vector<State>::const_iterator it = alive_states.begin(); it != alive_states.end(); it++)
       {
-        new_states.clear();
-        for(vector<State>::const_iterator it = alive_states.begin(); it != alive_states.end(); it++)
+        State s = *it;
+        s.step(alphabet(L"<$>"));
+        if(s.size() > 0)
         {
-          State s = *it;
-          s.step(alphabet(L"<$>"));
-          if(s.size() > 0)
-          {
-            new_states.push_back(s);
-          }
-
-          if(s.isFinal(anfinals))
-          {
-            out += s.filterFinals(anfinals, alphabet, escaped_chars);
-            new_states.push_back(*initial_state);
-          }
+          new_states.push_back(s);
         }
-        alive_states.swap(new_states);
 
-        outOfWord = true;
-        in += val;
-        continue;
-      }
-
-      if(val == L'<' && !outOfWord) // tag
-      {
-        wstring tag = readFullBlock(input, L'<', L'>');
-        if(!alphabet.isSymbolDefined(tag))
+        if(s.isFinal(anfinals))
         {
-          alphabet.includeSymbol(tag);
+          out += s.filterFinals(anfinals, alphabet, escaped_chars);
+          new_states.push_back(*initial_state);
         }
-        val = static_cast<int>(alphabet(tag));
-        in += tag;
-
-        // fwprintf(stderr, L"tag %S: %d\n", tag.c_str(), val);
-      }
-      else {
-        in += (wchar_t) val;
-        // wcout << (wchar_t) val << endl;
       }
 
-      if(!outOfWord)
+      alive_states.swap(new_states);
+      outOfWord = true;
+      in += val;
+      continue;
+    }
+
+    if(val == L'<' && !outOfWord) // tag
+    {
+      wstring tag = readFullBlock(input, L'<', L'>');
+      if(!alphabet.isSymbolDefined(tag))
       {
-        new_states.clear();
-        wstring res = L"";
-        for(vector<State>::const_iterator it = alive_states.begin(); it != alive_states.end(); it++)
-        {
-          res = L"";
-          State s = *it;
-          if(val < 0)
-          {
-            s.step_override(val, alphabet(L"<ANY_TAG>"), val);
-          }
-          else if(val > 0)
-          {
-            s.step_override(val, alphabet(L"<ANY_CHAR>"), val); // deal with cases!
-          }
-          if(s.size() > 0)
-          {
-            new_states.push_back(s);
-          }
-          // wcout << L"|   | " << /*(wchar_t) val << */L"val " << L"size: " << s.size() << L" final: " << s.isFinal(anfinals) << endl;
-          // wcerr << L"|   | cur: " << s.getReadableString(alphabet) << endl;
-          if(s.isFinal(anfinals))
-          {
-            // cout << "finals size: " << s.size() << endl;
-            out = s.filterFinals(anfinals, alphabet, escaped_chars);
-            // wcerr << s.getReadableString(alphabet) << endl;
-            new_states.push_back(*initial_state);
-            finalFound = true;
+        alphabet.includeSymbol(tag);
+      }
+      val = static_cast<int>(alphabet(tag));
+      in += tag;
+      // fwprintf(stderr, L"tag %S: %d\n", tag.c_str(), val);
+    }
+    else {
+      in += (wchar_t) val;
+      // wcout << (wchar_t) val << endl;
+    }
 
-            for (int i=0; i < (int) out.size(); i++)
+    if(!outOfWord)
+    {
+      new_states.clear();
+      wstring res = L"";
+      for(vector<State>::const_iterator it = alive_states.begin(); it != alive_states.end(); it++)
+      {
+        res = L"";
+        State s = *it;
+        if(val < 0)
+        {
+          s.step_override(val, alphabet(L"<ANY_TAG>"), val);
+        }
+        else if(val > 0)
+        {
+          s.step_override(val, alphabet(L"<ANY_CHAR>"), val); // deal with cases!
+        }
+        if(s.size() > 0)
+        {
+          new_states.push_back(s);
+
+        }
+        // wcout << L"|   | " << /*(wchar_t) val << */L"val " << L"size: " << s.size() << L" final: " << s.isFinal(anfinals) << endl;
+        // wcerr << L"|   | cur: " << s.getReadableString(alphabet) << endl;
+        if(s.isFinal(anfinals))
+        {
+          // cout << "finals size: " << s.size() << endl;
+          out = s.filterFinals(anfinals, alphabet, escaped_chars);
+          // wcerr << s.getReadableString(alphabet) << endl;
+          new_states.push_back(*initial_state);
+
+          reset = true;
+          finalFound = true;
+
+          for (int i=0; i < (int) out.size(); i++)
+          {
+            wchar_t c = out[i];
+            if(c == L'/')
             {
-              wchar_t c = out[i];
-              if(c == L'/')
-              {
-                out[i] = L'^';
-              }
-              else if(c == L'$')
-              {
-                out[i-1] = '$';
-                out[i] = L' ';
-                out[i+1] = L'^';
-              }
+              out[i] = L'^';
             }
-            out = out.substr(0, out.length()-2); // remove extra trailing ' ^
-            fputwc(L' ', output);
-            fputws(out.c_str(), output);
+            else if(c == L'$')
+            {
+              out[i-1] = '$';
+              out[i] = L' ';
+              out[i+1] = L'^';
+            }
           }
+          out = out.substr(0, out.length()-3); // remove extra trailing '$ ^
+                                               // '^ ' is excess, '$' will be added in the next loop with fputws(in,output)
+          // fputwc(L' ', output);
+          if(leading) {
+            fputwc(L' ', output);
+          }
+          fputws(out.c_str(), output);
         }
-        alive_states.swap(new_states);
       }
+      alive_states.swap(new_states);
 
-      if(outOfWord)
-      {
-        continue;
-      }
+    }
+
+    if(outOfWord)
+    {
+      continue;
+    }
   }
 
   // if(finalFound)
@@ -248,12 +278,14 @@ int main (int argc, char** argv)
   // else
   if (!finalFound)
   {
-    out = in;
+    // wcout << in << endl;
+  fputws(in.c_str(), output);
+    // wcout << in << endl;
     // wcout << out << endl;
     // wcout << "equals? " << (out==in);
   }
   // wcout << out.c_str() << endl;
-  // fputws(out.c_str(), output);
+
   fputwc(L'\n', output);
   return 0;
 }
