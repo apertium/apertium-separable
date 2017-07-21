1bbdf0e6bde85d83130501382e167497e3bb2f8a
Irene Tang <itang1@swarthmore.edu>
2017-07-21 04:39:37 +0000

    prints misses in evaluate, run-tests works
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/apertium-separable@80833 72bbbca6-d526-0410-a7d9-f06f51895060

--- testing/run-tests.sh
+++ testing/run-tests.sh
@@ -1,14 +1,10 @@
 
 CORP=../corpus/
+MODULE=../src/
 LANGS=english
 
-for lang in $LANGS; do 
-	# Just copy input to output, but this is where your module is going to go
-	# you're going to do cat $CORP"/"$lang.src | your-module.py DATA > $CORP"/"$lang.tst
-
-	cat $CORP"/"$lang.src > $CORP"/"$lang.tst
-
-	# Run the evaluation
+for lang in $LANGS; do
+	cat $CORP"/"$lang.src | $MODULE./lsx-proc $MODULE"/"$lang.bin > $CORP"/"$lang.tst
 	python3 evaluate.py $CORP"/"$lang.src $CORP"/"$lang.ref $CORP"/"$lang.tst
 done
 
